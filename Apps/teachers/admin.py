c1b5cc7827e536fb22170596f1cb5c2d5b8a4fbe
Juwairia Bakhteyar <your-github-juwairiabakhteyar2580@gmail.com>
2026-07-30 18:10:49 +0530

    Register Teacher model in Django Admin

--- Apps/teachers/admin.py
+++ Apps/teachers/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teacher
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "teacher_id",
+        "first_name",
+        "last_name",
+        "designation",
+        "mobile_number",
+        "status",
+    )
+
+    search_fields = (
+        "teacher_id",
+        "first_name",
+        "last_name",
+        "mobile_number",
+        "designation",
+    )
+
+    list_filter = (
+        "status",
+        "designation",
+    )
+
+    ordering = (
+        "teacher_id",
+    )
